0ff1aca05c27e6add37439b56cea47304fa3924d
Unknown <cristiano.j.andrade@gmail.com>
2017-10-04 23:08:54 -0300

    Ajustes dos depoimentos e da pagina festa do pijama

--- festa-pijama.php
+++ festa-pijama.php
@@ -4,16 +4,16 @@
     <div class="container">
 
         <div class="row espacamento_top">
-            <div class="col-md-12">
+            <div class="col-md-6">
                 <div class="titulo-grande text-color-marrom">
-                    <span class="nuvem-pijama">Festa do Pijama</span>
+                    <h1 class="nuvem-azul-capacete">Festa do pijama</h1>
                 </div>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-md-12">
-                <div class="espacamento_bottom text-normal text-color-marrom">
+            <div class="col-md-10">
+                <div class="espacamento_bottom text-normal text-color-marrom pijama-description">
                     Que criança não sonha com o dia em que todos os seus melhores amigos vão poder dormir na sua casa? A proposta da Nuvens de Papel para essa festa é aliar diversão e aconchego, tudo feito com muito cuidado e pensado carinhosamente para seu filho!
                 </div>
             </div>
@@ -21,7 +21,7 @@
 
         <div class="row">
             <div class="col-md-12">
-                <img class="img-responsive" src="img/nossa-festa-pijama.png" alt="">
+                <img class="img-responsive itens-pijama" src="img/nossa-festa-pijama.png" alt="">
             </div>
         </div>
 
@@ -37,9 +37,14 @@
         </div>
 
         <div class="espacamento_top espacamento_bottom">
-            <div class="titulo-grande text-color-marrom">
-                Fotos
-            </div>
+		 <div class="row">
+                <div class="col-md-12">
+					<div class="titulo-grande text-color-marrom">
+						Fotos
+					</div>
+				</div>
+			</div>
+			
             <div class="row">
                 <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
                     <?php require "fotos.php" ?>
@@ -49,10 +54,12 @@
 
         <div class="espacamento_top espacamento_bottom">
             <div class="row">
-                <div class="titulo-grande text-color-marrom">
-                    Parceiros
-                </div>
-                <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10">
+				<div class="col-md-12">
+					<div class="titulo-grande text-color-marrom">
+						Parceiros
+					</div>
+				</div>
+				<div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10">
                     <?php require "parceiros.php" ?>
                 </div>
             </div>
