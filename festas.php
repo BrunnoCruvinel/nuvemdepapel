e6d82ca7851519d444bc1a31e05f6cf6c3bbdfe5
Unknown <cristiano.j.andrade@gmail.com>
2017-10-05 23:33:32 -0300

    Ajuste festas

--- festas.php
+++ festas.php
@@ -65,9 +65,13 @@
 
 
         <div class="espacamento_top espacamento_bottom">
-            <div class="titulo-grande text-color-marrom">
-                Depoimentos
-            </div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="titulo-grande text-color-marrom">
+						Depoimentos
+					</div>
+				</div>
+			</div>
             <?php require "depoimentos.php" ?>
         </div>
 
@@ -186,22 +190,28 @@
         </div>
 
         <div class="espacamento_top espacamento_bottom">
-            <div class="titulo-grande text-color-marrom">
-                Fotos
-            </div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="titulo-grande text-color-marrom">
+						Fotos
+					</div>
+				</div>
+			</div>
             <div class="row">
-                <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+                <div class="col-md-10 col-sm-10 col-xs-10 fotos-carousel">
                     <?php require "fotos.php" ?>
                 </div>
             </div>
         </div>
 
         <div class="espacamento_top espacamento_bottom">
-            <div class="row">
-                <div class="titulo-grande text-color-marrom">
-                    Parceiros
-                </div>
-                <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="titulo-grande text-color-marrom">
+						Parceiros
+					</div>
+				</div>
+                <div class="col-md-10 col-sm-10 col-xs-10 fotos-carousel">
                     <?php require "parceiros.php" ?>
                 </div>
             </div>
