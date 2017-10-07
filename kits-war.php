0586be515a1a7796ca0a9b03d12a94096473b00e
Unknown <cristiano.j.andrade@gmail.com>
2017-10-07 17:44:55 -0300

    Criação de página de Kits War

---
+++ kits-war.php
@@ -0,0 +1,90 @@
+<?php require "header.php" ?>
+
+<section id="kit-war" class="bg-color-azul bg-nuvem-festa-pijama">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="titulo-grande text-color-marrom espacamento_top">
+                    Kits Festa War
+                </div>
+            </div>
+        </div>
+		
+		<div class="row">
+			<div class="col-md-8 col-sm-8">
+				<div class="row">
+					<div class="col-md-11">
+						<div class="text-normal text-color-marrom espacamento_bottom">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+							dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation.
+						</div>
+					</div>
+					<div class="col-md-5 text-center"></div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12 col-sm-12">
+						<div class="row espacamento_bottom">
+							<div id="kits-war-img" class="col-md-12">
+								<div class="item-slide">
+									<div class="row">
+										<div class="col-md-7 col-sm-7 col-xs-7 col-lg-7">	
+											<img src="img/foto_war_1.png" class="espacamento-kits-top" />
+											<img src="img/foto_war_3.png" class="espacamento-kits-top espacamento-kits-bottom" />
+											<img src="img/foto_war_1.png" class="espacamento-kits-top espacamento-kits-bottom" />
+										</div>
+										<div class="col-md-5 col-sm-5 col-xs-5 col-lg-5">	
+											<img src="img/foto_war_2.png" class="espacamento-kits-top" />
+											<img src="img/foto_war_4.png" class="espacamento-kits-top espacamento-kits-bottom" />
+											<img src="img/foto_war_2.png" class="espacamento-kits-top espacamento-kits-bottom" />
+										</div>
+									</div>
+								</div>
+
+                        
+							</div>
+						</div>
+					</div>
+            
+				</div>
+			</div>
+
+			<div class="col-md-4 col-sm-4 text-right">
+				<div class="espacamento_bottom">
+					<img src="img/foto_war_kit.png" alt="">
+				</div>
+				<img src="img/eu_quero_mas_nao_moro.png" alt="">
+				
+			</div>
+
+		</div>
+		
+		<div class="espacamento_top espacamento_bottom">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="titulo-grande text-color-marrom">
+						Depoimentos
+					</div>
+				</div>
+			</div>
+            <?php require "depoimentos.php" ?>
+        </div>
+
+        <div class="espacamento_top espacamento_bottom">
+            <div class="row">
+                <div class="titulo-grande text-color-marrom">
+                    Parceiros
+                </div>
+                <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+                    <?php require "parceiros.php" ?>
+                </div>
+            </div>
+        </div>
+		
+		<div id="home-casa" class="espacamento_top">
+		</div>
+    </div>
+
+</section>
+
+<?php require "footer.php" ?>
