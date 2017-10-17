486724ddfd863c0ddd852b1c84720bbe1fc5f901
Unknown <cristiano.j.andrade@gmail.com>
2017-10-16 21:49:17 -0300

    Remoção de imports desnecessarios

--- footer.php
+++ footer.php
@@ -58,7 +58,6 @@
    </section>
 
   	<script type="text/javascript" src="js/main.js"></script>
-	<script type="text/javascript" src="js/lightbox.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	
     <script src="js/plugins/owl-carousel/dist/owl.carousel.min.js"></script>
